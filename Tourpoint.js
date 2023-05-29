10d608f403de2582992cb6324db5782aabf03c37
siraphatsornK <124877975+siraphatsornK@users.noreply.github.com>
2023-05-29 11:34:52 +0700

    Add files via upload

---
+++ Tourpoint.js
@@ -0,0 +1,145 @@
+var tourpoint = {
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "ห้องทานข้าวสุพรรณิการ์ ",
+        "level": "⭐⭐⭐⭐⭐"
+      },
+      "geometry": {
+        "coordinates": [
+          100.49085571299435,
+          13.745312735014878
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "ร้านอรรถรส",
+        "level": "⭐⭐⭐⭐"
+      },
+      "geometry": {
+        "coordinates": [
+          100.571625137755,
+          13.737681475862928
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Bangkok Bold Kitchen",
+        "level": "⭐⭐⭐⭐⭐"
+      },
+      "geometry": {
+        "coordinates": [
+          100.54589586412084,
+          13.74398473352852
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "ร้านศรีตราด",
+        "level": "⭐⭐⭐⭐⭐"
+      },
+      "geometry": {
+        "coordinates": [
+          100.56811572751195,
+          13.736814107476505
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "เรือนมัลลิการ์",
+        "level": "⭐⭐⭐⭐⭐"
+      },
+      "geometry": {
+        "coordinates": [
+          100.56306913775205,
+          13.725189475941903
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "ร้านลูกไก่ทอง",
+        "level": "⭐⭐⭐"
+      },
+      "geometry": {
+        "coordinates": [
+          100.51117613775665,
+          13.72672815594241
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Praya Dining",
+        "level": "⭐⭐⭐"
+      },
+      "geometry": {
+        "coordinates": [
+          100.49351313775543,
+          13.766247893927073
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "ราก (RARK Authentic Thai Cuisine)",
+        "level": "⭐⭐⭐⭐"
+      },
+      "geometry": {
+        "coordinates": [
+          100.54386868502945,
+          13.679870320386954
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Quain Bangkok",
+        "level": "⭐⭐⭐⭐"
+      },
+      "geometry": {
+        "coordinates": [
+          100.58313313775483,
+          13.724450736256145
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "ภัตตาคารจันทร์เพ็ญ",
+        "level": "⭐⭐⭐⭐⭐"
+      },
+      "geometry": {
+        "coordinates": [
+          100.54766611122125,
+          13.724440714068521
+        ],
+        "type": "Point"
+      }
+    }
+  ]
+}
